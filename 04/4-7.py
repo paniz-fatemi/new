c957a288aa773f49fdfc9a0e6d6e954b4b2dc2e1
paniz fatemi <88180685+paniz-fatemi@users.noreply.github.com>
2021-08-11 01:18:33 +0430

    Update 4-7.py

--- 04/4-7.py
+++ 04/4-7.py
@@ -1,1 +1,17 @@
 #7
+def kmm(a,b):
+    if a > b:
+        c = a
+        a = b
+        b = c
+    while b!= 0:
+        o = a % b
+        a = b
+        b = o
+    r = a
+    return r
+while True:
+    x = int(input())
+    y = int(input())
+    p = int((x*y)/kmm(x,y))
+    print("kmm : ", p)
