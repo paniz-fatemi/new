97cf736cd37693a16a6d5a1bde56fbfd798780bb
paniz fatemi <88180685+paniz-fatemi@users.noreply.github.com>
2021-08-11 01:22:51 +0430

    Update 4-6.py

--- 04/4-6.py
+++ 04/4-6.py
@@ -1,16 +1,36 @@
 #6
-n = int(input())
-m = int(input())
-if m > n and m /n == m//n:
-    print("bmm is: " , n)
-elif n > m and n /m == n//m:
-    print("bmm is: " , m)
-elif ("blah blah blah"):
-    print("bmm is: ", 1)
+# n = int(input())
+# m = int(input())
+# if m > n and m /n == m//n:
+#     print("bmm is: " , n)
+# elif n > m and n /m == n//m:
+#     print("bmm is: " , m)
+# elif ("blah blah blah"):
+#     print("bmm is: ", 1)
 
 
 
-n = int(input())
-m = int(input())
-for i in range(n):
-    if 
+# n = int(input())
+# m = int(input())
+# for u in range(m):
+#     pass
+# for i in n:
+#     if (0,u) in (0,i):
+#         print("there is some  number but I do not no how  I can find out what  is it")
+#     elif (0,i) in (0,u):
+#         print("there is some  number but I do not no how  I can find out what  is it")
+#     else:
+#         print("no.")
+
+
+
+# n = int(input())
+# m = int(input())
+# MAXimum = max(n,m)
+# while MAXimum>1:
+#     if  MAXimum / n == MAXimum//n and MAXimum/m == MAXimum//m:
+#         MAXimum = MAXimum - 1
+#         print("yes")
+#     else:
+#         print("there is some  number but I do not no how  I can find out what  is it")
+#     break
