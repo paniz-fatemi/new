3720950e0c9b81cf8c0c6b719104f2b5cc1ea429
paniz fatemi <88180685+paniz-fatemi@users.noreply.github.com>
2021-08-11 19:35:12 +0430

    Update 4-6.py

--- 04/4-6.py
+++ 04/4-6.py
@@ -1,36 +1,8 @@
 #6
-# n = int(input())
-# m = int(input())
-# if m > n and m /n == m//n:
-#     print("bmm is: " , n)
-# elif n > m and n /m == n//m:
-#     print("bmm is: " , m)
-# elif ("blah blah blah"):
-#     print("bmm is: ", 1)
+a = int(input())
+b = int(input())
 
-
-
-# n = int(input())
-# m = int(input())
-# for u in range(m):
-#     pass
-# for i in n:
-#     if (0,u) in (0,i):
-#         print("there is some  number but I do not no how  I can find out what  is it")
-#     elif (0,i) in (0,u):
-#         print("there is some  number but I do not no how  I can find out what  is it")
-#     else:
-#         print("no.")
-
-
-
-# n = int(input())
-# m = int(input())
-# MAXimum = max(n,m)
-# while MAXimum>1:
-#     if  MAXimum / n == MAXimum//n and MAXimum/m == MAXimum//m:
-#         MAXimum = MAXimum - 1
-#         print("yes")
-#     else:
-#         print("there is some  number but I do not no how  I can find out what  is it")
-#     break
+for i in range(1,a+1):
+    if a % i == 0 and b% i == 0:
+        bmm = i
+print(bmm)
