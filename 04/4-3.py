135f137eb755b9fd724b8523cdb1fb2e9d31d7b3
paniz fatemi <88180685+paniz-fatemi@users.noreply.github.com>
2022-06-24 19:51:04 +0430

    Update 4-3.py

--- 04/4-3.py
+++ 04/4-3.py
@@ -1,26 +1,7 @@
 #3
-# x = input("enter x: " )
-# y = input("enter y: " )
-# try:
-#     x = int(x)
-#     y = int(y)
-#     for i in range(1, x + 1):
-#         print(i, end=' ')
-#     print()
-#     for i in range( 1, x + 1):
-#         print( i * y, end = ' ' )
-#     print()
-# except ValueError as err:
-#     print(err)
-
-
-
-
 x,y = eval(input("enter X,Y: "))
-for i in range(1,x+1):
-    for j in range(1,y+1):
+for i in range(1,y+1):
+    for j in range(1,x+1):
         print(format(i*j,'3d'),end='')
     print()
-    
-    
 #:(((
