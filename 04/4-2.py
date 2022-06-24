7c234e1b4d7042628eefc38e0e35767b7cf256f0
paniz fatemi <88180685+paniz-fatemi@users.noreply.github.com>
2022-06-24 19:49:33 +0430

    Update 4-2.py

--- 04/4-2.py
+++ 04/4-2.py
@@ -1,1 +1,14 @@
 #2
+def fucktoreal(nom):
+    num=1
+    t = 0
+    for i in range(nom):
+        num = num * (i+1)
+        if num == nom:
+            print("yes")
+            t=1
+            break
+    if t==0:
+        print("no")
+nom=int(input())
+fucktoreal(nom)
