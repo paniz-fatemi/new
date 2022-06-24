54ccf0782f1520784cd15d65211193d836a3e8b6
paniz fatemi <88180685+paniz-fatemi@users.noreply.github.com>
2022-06-24 19:52:00 +0430

    Update 4-5.py

--- 04/4-5.py
+++ 04/4-5.py
@@ -1,30 +1,15 @@
 #5
 
-# adad = int(input())
-# i = 1
-# factorial = 1
-# for i in range(adad):
-#     if factorial == factorial* i:
-#         print("yes")
-#     elif i < adad:
-#         break
-#         print("no")
-
-
-
-# adad = int(input())
-# factorial = 1
-# for i in (1, adad):
-#     if adad > i:
-#         a = adad - i
-#         i = adad - a
-#     if factorial == factorial * i:
-#         print("yes")
-#         break
-#     elif factorial != factorial * i:
-#         print("no")
-#         break
-
-
-
-# ناااااااااااقص
+def fucktoreal(nom):
+    num=1
+    t = 0
+    for i in range(nom):
+        num = num * (i+1)
+        if num == nom:
+            print("yes")
+            t=1
+            break
+    if t==0:
+            print("no")
+nom=int(input())
+fucktoreal(nom)
